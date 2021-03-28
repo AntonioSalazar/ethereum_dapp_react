9664dbfb270b6edf892ab89b4a23b9ed4ec57fa6
unknown <antonio.salazar.emrsn@gmail.com>
2021-03-27 22:58:28 -0600

    Test is ready and passing, contract is ready

--- test/TestMyMessage.test.js
+++ test/TestMyMessage.test.js
@@ -1,1 +1,16 @@
 var MyMessage = artifacts.require('MyMessage');
+
+let instance;
+let accounts;
+let owner;
+
+contract('MyMessage', async (accs) =>{
+    accounts = accs;
+    owner = accounts[0];
+})
+
+it('Can set a message', async() => {
+    let instance = await MyMessage.deployed();
+    await instance.setMessage('Message created!');
+    assert.equal(await instance.getMessage.call(), 'Message created!')
+})
