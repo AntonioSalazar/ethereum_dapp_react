a91844586de74919c32c4bdabf11939f9b7852fd
unknown <antonio.salazar.emrsn@gmail.com>
2021-03-30 01:00:07 -0600

    able to detect the metamask account change

--- client/simple-contract/src/App.js
+++ client/simple-contract/src/App.js
@@ -11,23 +11,28 @@ import MessageProvider from './context/MessageProvider';
 
 
 const App = () => {
-  
+
   const checkWeb3Provider = async() => {
-      let web3 = new Web3(Web3.givenProvider)
+      let web3 = new Web3(Web3.givenProvider);
+
       if (window.ethereum) {
         // use MetaMask's provider
         web3 = new Web3(window.ethereum);
         await window.ethereum.request({method: 'eth_requestAccounts'}); // get permission to access accounts
         const contractAddress = '0xf6e09b77560702d07472889472ab972735e699f6';
         const myMessageContract = new web3.eth.Contract(myMessageABI, contractAddress);
-        const accounts = await web3.eth.getAccounts();
-        console.log(accounts[0]);
+        var account = '';
+        window.ethereum.on('accountsChanged', function (accounts) {
+          console.log('accountsChanges',accounts[0]);
+    
+        });
       } else {
         console.warn("No web3 detected. Falling back to http://127.0.0.1:9545. You should remove this fallback when you deploy live",);
         // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
         web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:9545"),);
       }
   }
+
  useEffect(()  => {
     checkWeb3Provider()
   }, [])
