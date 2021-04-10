843c1f26543b9a70e60ef80003928b0bb278c978
unknown <antonio.salazar.emrsn@gmail.com>
2021-04-09 23:30:49 -0500

    added an event, this event will help detect the frontend app when the message has been sent to the blockchain

--- contracts/MyMessage.sol
+++ contracts/MyMessage.sol
@@ -4,6 +4,7 @@ pragma solidity >=0.4.22 <0.9.0;
 contract MyMessage{
 
     string public myMessage;
+    event MessageSent(string myMessage);
 
     function setMessage(string memory _message) public returns(string memory){
         myMessage = _message;
